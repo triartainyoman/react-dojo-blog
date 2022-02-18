44ca6ef209d66a0f2b51b6fde7bf0936c2d5504a
triartainyoman <triarta.art@gmail.com>
2022-02-18 22:26:31 +0800

    Reusing custom hook

--- src/BlogDetail.jsx
+++ src/BlogDetail.jsx
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFecth from "./useFetch";
 
 const BlogDetail = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFecth("http://localhost:8000/blogs/" + id);
 
   return (
-    <div className="blog-detail">
-      <h2>Blog Detail - {id}</h2>
+    <div className="blog-details">
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
